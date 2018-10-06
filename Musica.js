dc51950b9995e4f30c84367a2b7c0c34e6075f15
jairnet <ejargoty@unicauca.edu.co>
2018-10-06 12:10:38 -0500

    06102018

--- Musica.js
+++ Musica.js
@@ -23,14 +23,21 @@ function cardartistas(artistas){
   
 
 contenedor.empty();
-$.each(trackmus, function (index, item) { 
-    var aux= `"<div class='row'><div class='card' style='width': 18rem;>
-    <img class='card-img-top' src='.../100px180/' alt='Card image cap'>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div></div>"`; 
+$.each(trackmus, function (index, item) {
+    var algo = '#text';
+    console.log("algo"+item.image[0]["#text"])
+    var aux= `<div class="col-md-3"><div class='card' style="width: 16rem; height: 35rem">
+      <img class='card-img-top' style="height: 13rem" src=' ${item.image[3]["#text"]}' alt='Card image cap'>
+      <div class="card-body">
+      <div class="card-header" id="puestoFondo" >Puesto Nro: ${item["@attr"].rank}</div>
+      <h5 class="card-title text-center">${item.name}</h5>
+      <ul class="list-group list-group-flush"  >
+      <li class="list-group-item">Artista: ${item.artist.name}</li>
+      <li class="list-group-item">  Duracion: ${item.duration}</li>
+      <li class="list-group-item"> Escuchados: ${item.listeners}</li>
+     </ul>    
+      <a href="${item.artist.url}" class="btn btn-outline-danger">Mas</a>
+    </div></div></div>`; 
     contenedor.append(aux);
      
 });
